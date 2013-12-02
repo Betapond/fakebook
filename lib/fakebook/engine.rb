52dba9d85b45d7568a8eb6c7c132c4c864b33629
Jordan Rogers-Smith <jordan.rogers-smith@betapond.com>
2013-12-02 13:03:38 +0000

    remove uncessary factory girl

--- lib/fakebook/engine.rb
+++ lib/fakebook/engine.rb
@@ -7,9 +7,5 @@ module Fakebook
       app.middleware.use Fakebook::DataInjectorMiddleware
     end
 
-    config.generators do |g|
-      g.test_framework :rspec
-      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
-    end
   end
 end
